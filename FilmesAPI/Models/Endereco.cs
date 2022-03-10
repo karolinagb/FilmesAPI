d1c0811ff3ec99da5fa816a966033a57818c9660
x <karolina_bp@hotmail.com>
2022-03-10 14:49:32 -0300

    Usando o JsonIgnore para resolver o problema de informação cíclica com carregamento das propriedades na consulta

--- FilmesAPI/Models/Endereco.cs
+++ FilmesAPI/Models/Endereco.cs
@@ -1,5 +1,6 @@
 ﻿using FilmesAPI.Models;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace FilmesApi.Models
 {
@@ -11,6 +12,10 @@ namespace FilmesApi.Models
         public string Logradouro { get; set; }
         public string Bairro { get; set; }
         public int Numero { get; set; }
+
+        //[JsonIgnore] - Resolve o problema de informação cíclica
+        //Ignorar a propriedade Cinema de endereço no carregamento das propriedades
+        [JsonIgnore]
         public virtual Cinema Cinema { get; set; }
     }
 }
