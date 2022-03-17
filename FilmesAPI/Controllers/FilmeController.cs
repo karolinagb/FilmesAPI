fa6b9f4b1de3290273d998968dfb3ffde8a76a39
x <karolina_bp@hotmail.com>
2022-03-17 12:19:05 -0300

    Implementando autenticacao via token jwt projeto Filmes

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -1,6 +1,7 @@
 ﻿using FilmesAPI.Data.Dtos;
 using FilmesAPI.Services;
 using FluentResults;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,7 @@ namespace FilmesAPI.Controllers
         }
 
         [HttpPost]
+        [Authorize(Roles = "admin")]
         public ActionResult AdicionaFilme([FromBody] CreateFilmeDto filmeDto)
         {
             ReadFilmeDto readDto = _filmeService.AdicionaFilme(filmeDto);
--- FilmesAPI/Startup.cs
+++ FilmesAPI/Startup.cs
@@ -1,13 +1,16 @@
 using FilmesAPI.Data;
 using FilmesAPI.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
+using System.Text;
 using System.Text.Json.Serialization;
 
 namespace FilmesAPI
@@ -29,10 +32,38 @@ namespace FilmesAPI
                 .UseMySQL(Configuration
                 .GetConnectionString("FilmeConnection")));
 
+            services.AddAuthentication(auth =>
+            {
+                auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                //desafio para validar o token
+                auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(token =>
+            {
+                token.RequireHttpsMetadata = true;
+                token.SaveToken = true;
+                token.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    //validar quem está assinando/gerou esse token
+                    ValidateIssuerSigningKey = true,
+
+                    //definir como ele vai validar quem assinou esse token
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes("aaafdfdsfjdsfskdjbbbfjdfsdfksdfdjs")),
+
+                    //Não vamos nos preocupar quem é o issuer desse token
+                    //nem vamos valida a audience
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+
+                    //O relógio de contagem de tempo de expiração do token vai ser a partir de zero
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
             services.AddScoped<FilmeService, FilmeService>();
             services.AddScoped<CinemaService, CinemaService>();
 
-            services.AddControllers().AddJsonOptions(x=>
+            services.AddControllers().AddJsonOptions(x =>
             x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve); //Ignora a referencia ciclica
 
             //Parâmetros = informa que queremos usar o AutoMapper dentro do Assembly da nossa aplicação
@@ -54,6 +85,7 @@ namespace FilmesAPI
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
